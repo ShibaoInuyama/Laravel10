45b09b2cffc02e9db40e88af581bea300711d2f9
Yassine <yassine_oumakar@hotmail.com>
2023-12-07 11:51:33 +0100

    creation des controllers

--- site-salle-de-sport/app/Http/Controllers/MainController.php
+++ site-salle-de-sport/app/Http/Controllers/MainController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function AccueilController()
+    {
+        return view('accueil');
+    }
+
+    public function CoursController()
+    {
+        return view('cours');
+    }
+
+    public function Tarifscontroller()
+    {
+        return view('tarifs');
+    }
+
+    public function ContactController()
+    {
+        return view('contact');
+    }
+
+    public function MentionsLegalesController()
+    {
+        return view('mentions-legales');
+    }
 }
--- site-salle-de-sport/routes/web.php
+++ site-salle-de-sport/routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
+});
+
+Route::get('/', function () {
+    return view('cours');
+});
+
+Route::get('/', function () {
+    return view('tarifs');
+});
+
+Route::get('/', function () {
+    return view('contact');
+});
+
+Route::get('/', function () {
+    return view('mentions-legales');
 });

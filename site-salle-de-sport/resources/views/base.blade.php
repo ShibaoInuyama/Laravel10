bf5367a04df1c46f2c2121bc1f321711303decb7
Yassine <yassine_oumakar@hotmail.com>
2023-12-07 13:23:46 +0100

    modification d'une erreur

--- site-salle-de-sport/resources/views/base.blade.php
+++ site-salle-de-sport/resources/views/base.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/foo.css', 'resources/js/foo.js'])
+    <title>{{ config('app.Sport3000') }} - @yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    
+    <header>
+        <h1> <h1>
+    <nav>
+
+<ul>
+    <li>
+        <a href="{{ route ('main.home') }}">Acceuil</a>
+    </li>
+    <li>
+        <a href="{{ route ('main.menu') }}">Menu</a>
+    </li>
+</ul>
+</nav>
+    </header>
 </body>
 </html>
